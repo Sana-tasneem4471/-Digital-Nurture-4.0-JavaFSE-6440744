4e04781e804b716ce5074143aee4f291806e7a36
SK.SANA TASNEEM <159793464+Sana-tasneem4471@users.noreply.github.com>
2025-07-18 17:58:51 +0530

    Add files via upload

---
+++ DEEPSKILLING/WEEK_5/loan/src/main/java/com/example/loan/LoanController.java
@@ -0,0 +1,15 @@
+package com.example.loan;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoanController {
+
+	@GetMapping("/loans/{number}")
+	public Loan getLoanDetails(@PathVariable("number") String number) {
+	    return new Loan("H00987987972342", "car", 400000, 3258, 18);
+	}
+
+}
